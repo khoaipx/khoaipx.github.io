27d7c2cc9b458c0d7bbe776961c803460b8c8766
khoaipx <phamxuankhoai@gmail.com>
2024-07-30 15:32:15 +0700

    fix a tag

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,25 +25,19 @@ const Hero = () => {
 			<div className='flex items-center justify-center md:flex-row flex-col'>
 				<div className='rounded-full border-[3px] border-sh-blue relative scale-95 hover:scale-100 transition ease-in shadow-lg'>
 					<div className='h-44 w-44 select-none relative group'>
-						<a
-							href='https://www.linkedin.com/in/khoaipx93/'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							<Image
-								className='rounded-full w-full h-full'
-								src={UserImg}
-								alt='KhoaiAvatar'
-								layout='fill'
-								draggable='false'
-							/>
-							<div className='absolute bottom-0 flex-col items-center hidden mb-44 group-hover:flex'>
-								<span className='relative z-10 p-2 text-xs leading-none text-sh-white whitespace-no-wrap bg-sh-dark shadow-lg rounded-md'>
-									{"Hey, It's so nice to see you here, thanks for visiting !"}
-								</span>
-								<div className='w-3 h-3 -mt-2 rotate-45 bg-sh-dark'></div>
-							</div>
-						</a>
+						<Image
+							className='rounded-full w-full h-full'
+							src={UserImg}
+							alt='KhoaiAvatar'
+							layout='fill'
+							draggable='false'
+						/>
+						<div className='absolute bottom-0 flex-col items-center hidden mb-44 group-hover:flex'>
+							<span className='relative z-10 p-2 text-xs leading-none text-sh-white whitespace-no-wrap bg-sh-dark shadow-lg rounded-md'>
+								{"Hey, It's so nice to see you here, thanks for visiting !"}
+							</span>
+							<div className='w-3 h-3 -mt-2 rotate-45 bg-sh-dark'></div>
+						</div>
 					</div>
 				</div>
 				<div className='ml-0 md:ml-16'>
