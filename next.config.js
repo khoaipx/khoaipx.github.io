5ec085781aee57d218ba04126abaf2017118ecdb
khoaipx <phamxuankhoai@gmail.com>
2024-07-30 15:44:58 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true
-  }
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+}
 }
 
 module.exports = nextConfig
