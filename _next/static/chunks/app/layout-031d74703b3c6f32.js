d28d19dbaf2cdb1dc7f2712a4b016a31a6386ffe
khoaipx <phamxuankhoai@gmail.com>
2025-01-02 15:07:59 +0700

    Updates

---
+++ _next/static/chunks/app/layout-031d74703b3c6f32.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5287:(e,a,s)=>{Promise.resolve().then(s.t.bind(s,7100,23)),Promise.resolve().then(s.t.bind(s,9324,23))},9324:()=>{},7100:e=>{e.exports={style:{fontFamily:"'Cabin', 'Cabin Fallback'",fontStyle:"normal"},className:"__className_2f0af6"}}},e=>{var a=a=>e(e.s=a);e.O(0,[981,441,517,358],()=>a(5287)),_N_E=e.O()}]);
